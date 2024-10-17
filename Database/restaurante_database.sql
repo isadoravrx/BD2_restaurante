7687f54d3b881b429a840436e744f8a2a6138a3c
isadoravrx <isavitoriaxavier@gmail.com>
2024-10-17 09:53:06 -0300

    adicionando 3 tabelas

--- Database/restaurante_database.sql
+++ Database/restaurante_database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE restaurante;
 USE restaurante;
 
 CREATE TABLE IF NOT EXISTS cliente(
-id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(50),
 sexo CHAR,
 idade INT,
@@ -11,7 +11,7 @@ pontos INT
 );
 
 CREATE TABLE IF NOT EXISTS prato(
-id_prato INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(20),
 descricao VARCHAR(50),
 valor INT,
@@ -19,7 +19,38 @@ disponibilidade BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS fornecedor(
-id_fornecedor INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(50),
 estado_origem VARCHAR(20)
 );
+
+CREATE TABLE IF NOT EXISTS ingredientes(
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(50),
+data_fabricacao DATE,
+data_validade DATE,
+quantidade INT,
+observacao VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS usos(
+id_prato INT,
+id_ingrediente INT,
+FOREIGN KEY (id_prato) REFERENCES prato(id),
+FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id)
+);
+
+CREATE TABLE IF NOT EXISTS venda(
+id INT AUTO_INCREMENT PRIMARY KEY,
+id_cliente INT,
+id_prato INT,
+FOREIGN KEY (id_cliente) REFERENCES cliente(id),
+FOREIGN KEY (id_prato) REFERENCES prato(id),
+quantidade INT, 
+dia DATE, 
+hora TIME,
+valor REAL
+);
+
+
+
