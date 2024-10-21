3bf1ef04ad674ae1e6f47833cdea33ca79b6c56f
Marcelo <mhbcoelho99@gmail.com>
2024-10-21 19:52:05 -0300

    adding triggers 3/4

--- Database/restaurante_database.sql
+++ Database/restaurante_database.sql
@@ -180,3 +180,51 @@ GROUP BY c.id, c.nome
 ORDER BY total_gasto DESC;
 
 SELECT * FROM total_vendas_por_cliente;
+
+-- Triggers
+
+DELIMITER $$
+
+CREATE TRIGGER atualizar_pontos -- atualiza a quantidade de pontos
+AFTER INSERT ON venda
+FOR EACH ROW
+BEGIN
+    UPDATE cliente
+    SET pontos = pontos + FLOOR(NEW.valor / 10)
+    WHERE id = NEW.id_cliente;
+END$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE TRIGGER trigger_verificar_disponibilidade -- verifica a disponibilidade do prato
+BEFORE INSERT ON venda
+FOR EACH ROW
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM prato WHERE id = NEW.id_prato AND disponibilidade = TRUE) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Prato indisponível para compra'; 
+    END IF;
+END$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE TRIGGER trigger_reduzir_ingredientes -- reduz a quantidade dos ingredientes
+AFTER INSERT ON venda
+FOR EACH ROW
+BEGIN
+    UPDATE ingredientes
+    SET quantidade = quantidade - 1
+    WHERE id IN (
+        SELECT id_ingrediente 
+        FROM usos
+        WHERE id_prato = NEW.id_prato
+    );
+END$$
+
+DELIMITER ;
