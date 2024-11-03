8855560e8bc63af872491dba38e0afa370b68d90
Jose Almir <joseamfj@outlook.com>
2024-11-02 21:10:15 -0300

    Create stored procedure to pick a random client and add 100 points

--- Database/stored_procedures.sql
+++ Database/stored_procedures.sql
@@ -1,4 +1,4 @@
--- Reajuste - Receba um reajuste em percentual e aumente o valor de todos os pratos. 
+-- Reajuste: Receba um reajuste em percentual e aumente o valor de todos os pratos. 
 DELIMITER //
 
 CREATE PROCEDURE Reajuste(IN percentual DECIMAL(5, 2))
@@ -16,5 +16,28 @@ DELIMITER ;
 -- OBS: Como é descrito que AUMENTE e não ALTERE o valor, subentendesse que o percentual DEVE ser positivo.
 
 
+--Sorteio: Sorteie aleatoriamente um cliente, para que este cliente receba uma premiação de 100 pontos. 
+DELIMITER //
+CREATE PROCEDURE Sorteio()
+BEGIN
+	DECLARE cliente_id INT;
+    DECLARE pontuacao INT;
+    
+    SELECT id, pontos INTO cliente_id, pontuacao
+    FROM cliente
+    ORDER BY RAND()
+    LIMIT 1;
+	
+    UPDATE cliente
+    SET pontos = pontos + 100
+    WHERE id = cliente_id;
+    
+    SELECT cliente_id AS Cliente_Sorteado, 
+		   (SELECT nome 
+			FROM cliente 
+            WHERE id = cliente_id) AS Nome_Cliente,
+            pontuacao AS Pontuacao_Anterior,
+            (pontuacao + 100) AS Nova_Pontuacao;
+END//
 
-
+DELIMITER ;
